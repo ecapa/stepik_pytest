fbafe4a4fbfb2082e154e15ec0b755acbc682cd5
Eugen <eugen2005@gmail.com>
2022-01-07 16:16:31 +0200

    made changes

--- section3_lesson6/test_items.py
+++ section3_lesson6/test_items.py
@@ -1,4 +1,5 @@
 import time 
+from selenium.webdriver.common.by import By
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -7,7 +8,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_check_add_to_basket_btn(browser):
 	browser.get(link)
 	time.sleep(10)
-	add_to_basket_btn = browser.find_element_by_css_selector(".btn-add-to-basket")
+	add_to_basket_btn = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
 	time.sleep(3)
 	
 	assert add_to_basket_btn.is_displayed(), "No Button"
